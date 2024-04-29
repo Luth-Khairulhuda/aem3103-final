5068c48eb0c31e397c7f0fd1dbd44ab3ecb3bd93
L. Khairulhuda <khair027@umn.edu>
2024-04-29 01:12:43 -0500

    Adjusted variable names for problem 2, completed problem 3, and started working on problem 4

--- PaperPlane.m
+++ PaperPlane.m
@@ -24,32 +24,78 @@
 %   2a) Varying Inital Velocity
     figure
     subplot(1,2,1)
-    xlabel('Range, m'), ylabel('Height, m'), grid
+    xlabel('Range, m', 'FontSize',15), ...
+        ylabel('Height, m', 'FontSize',15), grid
     hold on
 
     Vo = [2, 3.55, 7.5];
     lines = ["r-", "k-", "g-"];
     for i = 1:length(Vo)
 	    xo		=	[Vo(i);Gam;H;R];
-	    [ta,xa]	=	ode23('EqMotion',tspan,xo);
-        plot(xa(:,4),xa(:,3), lines(i));
+	    [t,x]	=	ode23('EqMotion',tspan,xo);
+        plot(x(:,4),x(:,3), lines(i));
         hold on
     end
 
 %   2b) Varying Initial Flight Path Angle
     subplot(1,2,2)
-    xlabel('Range, m'), ylabel('Height, m'), grid
+    xlabel('Range, m', 'FontSize',15), ...
+        ylabel('Height, m', 'FontSize',15), grid
     hold on
 
     Gam_o = [-0.5, -0.18, 0.4];
     for i = 1:length(Gam_o)
 	    xo		=	[V;Gam_o(i);H;R];
-	    [ta,xa]	=	ode23('EqMotion',tspan,xo);
-        plot(xa(:,4),xa(:,3), lines(i));
+	    [t,x]	=	ode23('EqMotion',tspan,xo);
+        plot(x(:,4),x(:,3), lines(i));
         hold on
     end
 	
+%   3) 100 Tests
+    V_min = 2;
+    V_max = 7.5;
+    Gam_min = -0.5;
+    Gam_max = 0.4;
+    tspan = linspace(to, tf, 100);
+    drl = 100;
+    range = nan*zeros(100);
+    height = nan*zeros(100);
+    
+    figure
+    hold on
+    for i = 1:drl
+        rand_V = V_min + (V_max-V_min)*rand(1);
+        rand_Gam = Gam_min + (Gam_max-Gam_min)*rand(1);
+        xo		=	[rand_V;rand_Gam;H;R];
+	    [t,x]	=	ode23('EqMotion',tspan,xo);
+        plot(x(:,4),x(:,3), "k-")
 
+        % For Part 4
+        range(:,i) = x(:,4);
+        height(:,i) = x(:,3);
+
+    end
+    title("Trajectories of 100 Random Initial Velocities and Flight Path Angles")
+    xlabel('Range, m', 'FontSize',15), ...
+        ylabel('Height, m', 'FontSize',15), grid
+
+
+%   4) Average Trajectory
+    time = tspan;
+    avg_range = nan*(1:100);
+    avg_height = nan*(1:100);
+
+    for i = 1:drl
+        avg_range(i) = mean(range(i,:));
+        avg_height(i) = mean(height(i,:));
+    end
+
+    plot(avg_range,avg_height, "r-", "LineWidth", 5);
+
+    xo		=	[V;Gam;H;R];
+    [t,x]	=	ode23('EqMotion',tspan,xo);
+
+    plot(x(:,4),x(:,3), "b-", "LineWidth", 5);
 
 
 % %	b) Oscillating Glide due to Zero Initial Flight Path Angle
