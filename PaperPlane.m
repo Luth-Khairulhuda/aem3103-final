0cbddaf47a85d52e8cbf8eb336226fe692061350
L. Khairulhuda <khair027@umn.edu>
2024-04-28 22:31:54 -0500

    Fixed trajectory legend and added setup file to clean up main PaperPlane sim

--- PaperPlane.m
+++ PaperPlane.m
@@ -2,28 +2,9 @@
 %	Copyright 2005 by Robert Stengel
 %	August 23, 2005
 
-	global CL CD S m g rho	
-	S		=	0.017;			% Reference Area, m^2
-	AR		=	0.86;			% Wing Aspect Ratio
-	e		=	0.9;			% Oswald Efficiency Factor;
-	m		=	0.003;			% Mass, kg
-	g		=	9.8;			% Gravitational acceleration, m/s^2
-	rho		=	1.225;			% Air density at Sea Level, kg/m^3	
-	CLa		=	3.141592 * AR/(1 + sqrt(1 + (AR / 2)^2));
-							% Lift-Coefficient Slope, per rad
-	CDo		=	0.02;			% Zero-Lift Drag Coefficient
-	epsilon	=	1 / (3.141592 * e * AR);% Induced Drag Factor	
-	CL		=	sqrt(CDo / epsilon);	% CL for Maximum Lift/Drag Ratio
-	CD		=	CDo + epsilon * CL^2;	% Corresponding CD
-	LDmax	=	CL / CD;			% Maximum Lift/Drag Ratio
-	Gam		=	-atan(1 / LDmax);	% Corresponding Flight Path Angle, rad
-	V		=	sqrt(2 * m * g /(rho * S * (CL * cos(Gam) - CD * sin(Gam))));
-							% Corresponding Velocity, m/s
-	Alpha	=	CL / CLa;			% Corresponding Angle of Attack, rad
 	
 %	a) Equilibrium Glide at Maximum Lift/Drag Ratio
-	H		=	2;			% Initial Height, m
-	R		=	0;			% Initial Range, m
+    [V, Gam, H, R] = setup_sim();
 	to		=	0;			% Initial Time, sec
 	tf		=	6;			% Final Time, sec
 	tspan	=	[to tf];
@@ -45,12 +26,12 @@
 	figure
 	plot(xa(:,4),xa(:,3),xb(:,4),xb(:,3),xc(:,4),xc(:,3),xd(:,4),xd(:,3))
 	xlabel('Range, m'), ylabel('Height, m'), grid
+    legend("Trajectory Max L/D");
 
 	figure
 	subplot(2,2,1)
 	plot(ta,xa(:,1),tb,xb(:,1),tc,xc(:,1),td,xd(:,1))
-	xlabel('Time, s'), ylabel('Velocity, m/s'), grid
-    legend("Trajectory"); 
+	xlabel('Time, s'), ylabel('Velocity, m/s'), grid 
 	subplot(2,2,2)
 	plot(ta,xa(:,2),tb,xb(:,2),tc,xc(:,2),td,xd(:,2))
 	xlabel('Time, s'), ylabel('Flight Path Angle, rad'), grid
@@ -60,3 +41,8 @@
 	subplot(2,2,4)
 	plot(ta,xa(:,4),tb,xb(:,4),tc,xc(:,4),td,xd(:,4))
 	xlabel('Time, s'), ylabel('Range, m'), grid
+
+% Notes:
+% 3) randomly pick velocity and flight path angle in their range
+% and simulate all 100
+% Other) Can remove the parameter figures (4 subpots)
